3d6909554bd2a95a5a3d57e085d53a40b55a9f2e
Vivian Vesga <vivianvesga@gmail.com>
2021-01-03 13:26:15 -0500

    Installing material ui and material icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
-import React from "react"
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <React.Fragment>
+      <CssBaseline />
+    </React.Fragment>
+  )
 }
